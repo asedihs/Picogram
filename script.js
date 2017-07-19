9ad4539e7de6f07dcaa9eb279e17127ec2618da0
asedihs <aleksejs.sedihs@gmail.com>
2017-07-19 16:54:49 +0300

    adding initial files

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+//import './node_modules/font-awesome/css/font-awesome.css';
+import './style.less';
+
+//import Starter from './scripts/starter';
+
+//const rootElement = document.getElementById('main');
+//let starter = new Starter(rootElement);
+
+let div = document.getElementById('picogram');
+console.log(typeof(div));
+div.innerHTML = 'picogram';
